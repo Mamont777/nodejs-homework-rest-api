1418736f4176f5f22647bd9887ca8c794652df13
Sergii Podobrii <podobri@ukr.net>
2023-07-26 21:22:14 +0300

    fixing

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,14 +8,14 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
-    if (!user || !user.token || user.token !== token) {
+    if (!user.token) {
       next(HttpError(401));
     }
     req.user = user;
